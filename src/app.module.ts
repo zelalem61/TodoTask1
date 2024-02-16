5e30d540ad538bc491abaf46849d1fef9f650679
Zelalem Habtamu <108578495+zelalem61@users.noreply.github.com>
2024-02-16 02:42:26 -0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './todo/task.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/task')],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/task'),TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
